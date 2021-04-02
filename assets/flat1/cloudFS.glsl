7af6801f8eba9a2c4aaa4e8c4365d865d3ce9f3d
Dzung Nguyen <dzungng@seas.upenn.edu>
2021-04-02 10:47:01 -0400

    Changed cumulus cloud brightness

--- assets/flat1/cloudFS.glsl
+++ assets/flat1/cloudFS.glsl
@@ -13,6 +13,6 @@ void main(void) {
 
     // sum
     alpha = diffuse.x; // take alpha from red channel
-    color = vec4(vec3(1.0, 1.0, 1.0), alpha); // pre multiply alpha
+    color = vec4(vec3(1.0, 1.0, 1.0), alpha);
     gl_FragColor = color;
 }
